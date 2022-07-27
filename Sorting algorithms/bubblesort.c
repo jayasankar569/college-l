9092a8af3558e70f29c76f4dcab70fcefd19809c
jays56 <94859351+jays56@users.noreply.github.com>
2022-07-27 15:29:45 +0530

    Update bubblesort.c

--- Sorting algorithms/bubblesort.c
+++ Sorting algorithms/bubblesort.c
@@ -1,34 +1,68 @@
+// Program - Bubble Sort
 #include <stdio.h>
+#include<stdlib.h>
+#include <time.h>
+int array[1000000];
+
+float timedifference_msec(struct timeval t0, struct timeval t1)
+{
+    return (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
+}
 
 int main()
 {
-  int array[100], n, c, d, swap;
-
-  printf("Enter number of elements\n");
-  scanf("%d", &n);
-
-  printf("Enter %d integers\n", n);
-
-  for (c = 0; c < n; c++)
-    scanf("%d", &array[c]);
-
-  for (c = 0 ; c < n - 1; c++)
-  {
-    for (d = 0 ; d < n - c - 1; d++)
-    {
-      if (array[d] > array[d+1]) /* For decreasing order use '<' instead of '>' */
-      {
-        swap       = array[d];
-        array[d]   = array[d+1];
-        array[d+1] = swap;
-      }
-    }
-  }
-
-  printf("Sorted list in ascending order:\n");
-
-  for (c = 0; c < n; c++)
-     printf("%d\n", array[c]);
-
-  return 0;
+    struct timeval t0;
+    struct timeval t1;
+    float elapsed;
+    
+	int i,key,ch;
+	long size,limit;
+	printf("Enter number of Elements: ");
+	scanf("%ld",&size);
+	int array[size];
+	printf("Enter Upper limit in place values: ");
+	scanf("%ld",&limit);
+	
+	int num=size;
+	int j,x,y,temp;
+	srand( (unsigned) time(NULL) * getpid());
+	gettimeofday(&t0, NULL);
+	if(array != NULL)
+	{
+		for(j = 0; j < num; j++)
+		{
+			array[j] = rand()%limit;
+		}
+	}
+	gettimeofday(&t1, NULL);
+	elapsed = timedifference_msec(t0, t1);
+	printf("\n");
+	printf("List of Numbers:\n");
+	for(j = 0; j < num; j++)
+	{
+		printf("%d\n",array[j]);
+	}				
+	printf("Random code generated in %f milliseconds.\n", elapsed);
+    printf("\n");
+    gettimeofday(&t0, NULL);
+    for(x = 0; x < num - 1; x++)
+	{       
+		for(y = 0; y < num - x - 1; y++)
+		{          
+		    if(array[y] > array[y + 1])
+		    {               
+		        temp = array[y];
+		        array[y] = array[y + 1];
+		        array[y + 1] = temp;
+		    }
+		}
+	}
+    gettimeofday(&t1, NULL);
+    elapsed = timedifference_msec(t0, t1);
+    printf("Bubble sorted List of Numbers:\n");
+	for(j = 0; j < num; j++)
+	{
+		printf("%d\n",array[j]);
+	}
+   printf("Bubble sort executed in %f milliseconds.\n", elapsed);
 }
